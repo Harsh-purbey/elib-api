d492c15ebc5c107923c5044eb4f2ece57fa34d34
Harsh Purbey <harshpurbey8182@gmail.com>
2026-01-18 12:01:28 +0530

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Welocme to elib apis",
+  });
+});
+
+export default app;
