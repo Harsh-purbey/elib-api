a1b2372806b66c4407a68a36164f083ca42bfc06
Harsh Purbey <harshpurbey8182@gmail.com>
2026-01-18 17:06:51 +0530

    add user model

--- src/user/user-controller.js
+++ src/user/user-controller.js
@@ -1,10 +1,17 @@
 import createHttpError from "http-errors";
+import userModel from "./user-model";
 
 const createUser = async (request, response, next) => {
   const { name, email, password } = request.body;
   // Validation
   if (!name || !email || !password) {
-    const error = createHttpError(400, "All fields are required");
+    const error = createHttpError(400, "All fields are required.");
+    return next(error);
+  }
+  //Database call
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email.");
     return next(error);
   }
   // Process
