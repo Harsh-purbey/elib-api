5913f47b4c85f4f3ab206e08822d6ae3d7555459
Harsh Purbey <harshpurbey8182@gmail.com>
2026-01-19 23:19:16 +0530

    create book

--- src/book/book-controller.js
+++ src/book/book-controller.js
@@ -1,11 +1,12 @@
 import path from "path";
 import cloudinary from "../config/cloudinary.js";
 import createHttpError from "http-errors";
+import bookModel from "./book-model.js";
+import fs from "fs";
 
 const createBook = async (request, response, next) => {
 
-	console.log('FILES', request.files);
-
+	const { title, genre } = request.body
 
 	try {
 		const coverImageMimeType = request.files.coverImage[0].mimetype.split('/').at(-1);
@@ -29,13 +30,31 @@ const createBook = async (request, response, next) => {
 			format: 'pdf'
 		})
 
+		const newBook = await bookModel.create({
+			title, genre, author: '696cd6234e492c5026108ed7', coverImage: uploadResult.secure_url, file: bookFileUploadResult.secure_url
+		})
+
+		// delete temp files
+
+		try {
+			await fs.promises.unlink(filePath);
+			await fs.promises.unlink(bookFilePath);
+			return response.status(201).json({ id: newBook._id });
+
+		} catch (error) {
+         return next(createHttpError(500,"Error while deleting temporary store files."))
+		}
+
+
+
+
+
+
 	} catch (error) {
 		console.log(error);
 		return next(createHttpError(500, "Error while uploading the files."))
 	}
 
-
-	return response.json({});
 };
 
 export { createBook };
