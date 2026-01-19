5b8292a390d37b478709cfe6022f601220591e84
Harsh Purbey <harshpurbey8182@gmail.com>
2026-01-19 23:07:24 +0530

    upload files to cloudinary

--- src/book/book-controller.js
+++ src/book/book-controller.js
@@ -1,4 +1,40 @@
+import path from "path";
+import cloudinary from "../config/cloudinary.js";
+import createHttpError from "http-errors";
+
 const createBook = async (request, response, next) => {
+
+	console.log('FILES', request.files);
+
+
+	try {
+		const coverImageMimeType = request.files.coverImage[0].mimetype.split('/').at(-1);
+		const fileName = request.files.coverImage[0].filename;
+		const filePath = path.resolve(process.cwd(), "public/data/uploads", fileName)
+
+		const uploadResult = await cloudinary.uploader.upload(filePath, {
+			filename_override: fileName,
+			folder: "book-covers",
+			format: coverImageMimeType
+		})
+
+
+		const bookFileName = request.files.file[0].filename;
+		const bookFilePath = path.resolve(process.cwd(), "public/data/uploads", bookFileName)
+
+		const bookFileUploadResult = await cloudinary.uploader.upload(bookFilePath, {
+			resource_type: "raw",
+			filename_override: bookFileName,
+			folder: "book-pdfs",
+			format: 'pdf'
+		})
+
+	} catch (error) {
+		console.log(error);
+		return next(createHttpError(500, "Error while uploading the files."))
+	}
+
+
 	return response.json({});
 };
 
